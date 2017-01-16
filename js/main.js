2d011ebfcafcbe784c4d13af478109ca059c021c
MaryoryChavez <maryory.chavez@gmail.com>
2017-01-16 12:14:09 -0500

    archivos subidos

---
+++ js/main.js
@@ -0,0 +1,10 @@
+    document.getElementById("form-signin-heading").innerHTML="Por favor Inicia Sesión";
+
+    document.getElementsByClassName("form-control")[0].placeholder = "Correo Electronico";
+        document.getElementsByClassName("form-control")[1].placeholder = "Contraseña";
+
+
+    var etiqspa = document.getElementsByTagName("SPAN")[0];
+    etiqspa.innerHTML="Recordar Datos";
+    
+    document.getElementsByTagName("BUTTON")[0].innerHTML = "Iniciar Seción";
